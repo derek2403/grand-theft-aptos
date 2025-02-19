5540ce0db655b3ceb15559c1b6607c5ee52fb8c6
JingYuan <jingyuan0926@gmail.com>
2025-02-19 15:57:38 +0800

    Added show wallet info

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import { WalletSelector } from "../components/connectwallet/WalletSelector";
+import { WalletInfo } from "../components/WalletInfo";
+
 
 
 export default function Home() {
   return (
     <div className="p-4">
       <WalletSelector />
-
+      <WalletInfo/>
     </div>
   );
 }
